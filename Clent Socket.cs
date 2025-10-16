0a447192d1ab2c52f6cc02df3bfe3815d2a71d46
khadijakamran1326 <khadijakamran746@gmail.com>
2025-10-16 12:20:00 +0500

    Add files via upload

---
+++ Clent Socket.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+using System.Net.Sockets;
+using System.Text;
+class Client
+{
+    static void Main()
+    {
+        var client = new TcpClient("127.0.0.1", 5000);
+        var ns = client.GetStream();
+        Console.Write("Enter number:");
+        string num = Console.ReadLine();
+        ns.Write(Encoding.UTF8.GetBytes(num));
+        byte[] data = new byte[4096];
+        int bytes = ns.Read(data, 0, data.Length);
+        Console.WriteLine("\n" +
+            Encoding.UTF8.GetString(data, 0, bytes));
+        ns.Close();
+        client.Close();
+    }
+}
---
+++ Server.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+class server
+{
+    static void Main()
+    {
+        var listener = new TcpListener(IPAddress.Any, 5000);
+        listener.Start();
+        Console.WriteLine("Server running");
+        while (true)
+        {
+            var client = listener.AcceptTcpClient();
+
+            var ns = client.GetStream();
+            byte[] data = new byte[1024];
+            int bytes = ns.Read(data, 0, data.Length);
+            int n = int.Parse(Encoding.UTF8.GetString(data, 0, bytes));
+            string result = "";
+            for (int i = 1; i <= 10; i++)
+                result += $"{n}x{i}={n * i}\n";
+
+            ns.Write(Encoding.UTF8.GetBytes(result));
+            ns.Close();
+            client.Close();
+            Console.WriteLine($"Tablefor {n} sent to client");
+
+        }
+    }
+}
